d6fa54c4de5c97077c63f7955026ab1a541a4f2c
EricNoh <dosungroh@inslab.co.kr>
2018-01-07 14:56:14 +0900

    fh_send_data read line

--- fh_send_data.py
+++ fh_send_data.py
@@ -1,35 +1,44 @@
-#!/bin/python
-
+#!/usr/bin/python
+import time
 import csv
 
 class Datafile:
     __post_url = 'http://charger.tetrapacific.com/api/api-collector.php'
 
     def __init__(self, filename):
-        self.file = open(filename, 'r')
-        self.reader = csv.reader(self.file, delimiter=',', quotechar='|')
+        self.__file = open(filename, 'r')
+        self.__reader = csv.reader(self.__file, delimiter=',', quotechar='|')
+        self.__reader.next()
 
     def __del__(self):
-        if self.file is None:
-            self.file.close()
+        if self.__file is None:
+            self.__file.close()
 
-    def __getline():
-        row = self.reader.next()
+    def __getline(self):
+        try:
+            row = self.__reader.next()
+        except Exception as e:
+            print 'eof ------>'
+            self.__file.seek(0)
+            self.__reader.next()
+            row = self.__reader.next()
         return row
-    def post_data():
+
+    def post_data(self):
         row = self.__getline()
         print row
 
 
 def main():
     datafiles = []
-    datafiles.append('upload-foghorn-ac-10022001.csv')
-    datafiles.append('upload-foghorn-dc-10011103.csv')
-    datafiles.append('upload-foghorn-dc-10021101.csv')
-    while:
+    datafiles.append(Datafile('upload-foghorn-ac-10022001.csv'))
+    datafiles.append(Datafile('upload-foghorn-dc-10011103.csv'))
+    datafiles.append(Datafile('upload-foghorn-dc-10021101.csv'))
+    print datafiles
+    while True:
         for datafile in datafiles:
             datafile.post_data()
-        sleep(5)
+        time.sleep(1)
 
 if __name__ == '__main__':
     main()
