c7093d0e5739d42f3f194de032a1e509ec7c68eb
EricNoh <dosungroh@inslab.co.kr>
2018-01-07 14:15:37 +0900

    fh_send_data 수정

--- fh_send_data.py
+++ fh_send_data.py
@@ -1,1 +1,36 @@
 #!/bin/python
+
+import csv
+
+class Datafile:
+    __post_url = 'http://charger.tetrapacific.com/api/api-collector.php'
+
+    def __init__(self, filename):
+        self.file = open(filename, 'r')
+        self.reader = csv.reader(self.file, delimiter=',', quotechar='|')
+
+    def __del__(self):
+        if self.file is None:
+            self.file.close()
+
+    def __getline():
+        row = self.reader.next()
+        return row
+    def post_data():
+        row = self.__getline()
+        print row
+
+
+def main():
+    datafiles = []
+    datafiles.append('upload-foghorn-ac-10022001.csv')
+    datafiles.append('upload-foghorn-dc-10011103.csv')
+    datafiles.append('upload-foghorn-dc-10021101.csv')
+    while:
+        for datafile in datafiles:
+            datafile.post_data()
+        sleep(5)
+
+if __name__ == '__main__':
+    main()
+
