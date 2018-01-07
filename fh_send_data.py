1c9a2b30a98a1674d1e8c94d8896e689f5c69e34
EricNoh <dosungroh@inslab.co.kr>
2018-01-07 16:21:32 +0900

    fh_send_data 20180107

--- fh_send_data.py
+++ fh_send_data.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 import time
 import csv
+import requests
+
+'''
+pip install requests
+'''
+
 
 class Datafile:
-    __post_url = 'http://charger.tetrapacific.com/api/api-collector.php'
+    # __POST_ENDPOINT = 'http://charger.tetrapacific.com/api/api-collector.php'
+    __POST_URL = 'http://localhost:8080/post-to-me'
 
     def __init__(self, filename):
         self.__file = open(filename, 'r')
@@ -18,7 +25,6 @@ class Datafile:
         try:
             row = self.__reader.next()
         except Exception as e:
-            print 'eof ------>'
             self.__file.seek(0)
             self.__reader.next()
             row = self.__reader.next()
@@ -26,7 +32,37 @@ class Datafile:
 
     def post_data(self):
         row = self.__getline()
-        print row
+        #print row
+        data = {
+            'charger_id': row[0],
+            'gun_a_status': row[1],
+            'gun_b_status': row[2],
+            'gun_a_power': row[3],
+            'gun_b_power': row[4],
+            'gun_a_qty': row[5],
+            'gun_b_qty': row[6],
+            'gun_a_soc': row[7],
+            'gun_b_soc': row[8],
+            'alarm_old': row[9],
+            'phase_a_volt_under': row[10],
+            'phase_b_volt_under': row[11],
+            'phase_c_volt_under': row[12],
+            'phase_a_volt_over': row[13],
+            'phase_b_volt_over': row[14],
+            'phase_c_volt_over': row[15],
+            'phase_a_curr_over': row[16],
+            'phase_b_curr_over': row[17],
+            'phase_c_curr_over': row[18]
+        }
+        headers = {
+            'Content-type': 'application/x-www-form-urlencoded'
+        }
+        try:
+            res = requests.post(url=self.__POST_URL,
+                                headers=headers, data=data, timeout=15 * 60)
+            #print res.text
+        except Exception as e:
+            print e
 
 
 def main():
@@ -34,12 +70,12 @@ def main():
     datafiles.append(Datafile('upload-foghorn-ac-10022001.csv'))
     datafiles.append(Datafile('upload-foghorn-dc-10011103.csv'))
     datafiles.append(Datafile('upload-foghorn-dc-10021101.csv'))
-    print datafiles
+
     while True:
         for datafile in datafiles:
             datafile.post_data()
-        time.sleep(1)
+        time.sleep(5)
+
 
 if __name__ == '__main__':
     main()
-
---
+++ server/server.js
@@ -0,0 +1,27 @@
+'use strict'
+ 
+const express = require('express')
+const bodyParser = require('body-parser')
+ 
+// Create a new instance of express
+const app = express()
+ 
+// Tell express to use the body-parser middleware and to not parse extended bodies
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// Route that receives a POST request to /sms
+app.post('/post-to-me', function (req, res) {
+  const body = JSON.stringify(req.body)
+  console.log(req.body)
+  res.set('Content-Type', 'text/plain')
+  res.send(`You sent: ${body} to Express`)
+})
+ 
+// Tell our app to listen on port 8080
+app.listen(8080, function (err) {
+  if (err) {
+    throw err
+  }
+ 
+  console.log('Server started on port 8080')
+})
